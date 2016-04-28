421a6bdf4dbfd6bbbeab9a58c09c3baad0c5222c
ManuJS <emanuelle.menali@hotmail.com>
2016-04-28 12:57:21 -0300

    first commit

---
+++ calculadora.py
@@ -0,0 +1,26 @@
+num1 = int(input("informe um numero: "))
+num2 = int(input ("informe outro numero "))
+print ("1 - soma")
+print ("2 - subtração")
+print ("3 - multiplicação")
+print ("4 - divisão")
+
+
+funcao = int(input("escolha uma das opções de calculo: "))
+
+if funcao == 1:
+	soma = num1 + num2
+	print (soma)
+elif funcao == 2:
+	sub = num1 - num2
+	print (sub)
+elif funcao ==3:
+	mult = num1 * num2
+	print (mult)
+elif funcao == 4:
+	divis = num1 / num2
+	print (divis)
+
+else:
+	print ("opção invalida")
+
---
+++ hello.py
@@ -0,0 +1,8 @@
+nota = int(input("Digite sua nota: "))
+	
+if nota < 5:
+    print ("Você está reprovado")
+elif nota < 7: 
+	print ("Você está em recuperação")
+else:
+    print ("você foi aprovado")
